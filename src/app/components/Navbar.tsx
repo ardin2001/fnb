25c6f9e3bd7a84cd2b586ffbd6566a19d6d5e7c0
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-06-19 20:13:55 +0700

    update landing page

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,13 +6,13 @@ export default function Navbar() {
     const [status, setStatus]: any = UseStatus();
 
     return (
-        <div className="bg-white grid py-2 md:grid-flow-col md:grid-cols-3 md:items-center w-89/100 md:py-4 lg:w-85/100 xl:w-4/5 mx-auto">
-            <div className="grid grid-flow-col justify-between md:col-span-2">
+        <div className="bg-white grid py-2.5 md:grid-flow-col md:grid-cols-3 md:items-center px-6 sm:px-10 md:px-16 lg:px-24 xl:px-36 mx-auto drop-shadow-low sticky top-0 z-50">
+            <div className="grid grid-flow-col justify-between items-center md:col-span-2">
                 <h3 className="text-secondary text-xl font-bold">E-voting App</h3>
-                <IoMdMenu className="text-2xl sm:py-2.5 text-secondary md:hidden" onClick={setStatus} />
+                <IoMdMenu className="text-2xl sm:text-3xl text-secondary md:hidden" onClick={setStatus} />
             </div>
             <div className={`${status ? "block" : "hidden"} md:block url text-center`}>
-                <ul className="grid gap-3 pt-2 pb-3 md:pt-0 md:pb-0 md:grid-flow-col">
+                <ul className="grid gap-3 pt-2 pb-3 md:pt-0 md:pb-0 md:grid-flow-col md:justify-between">
                     <li className="font-normal text-sm hover:font-semibold">Home</li>
                     <li className="font-normal text-sm hover:font-semibold">About</li>
                     <li className="font-normal text-sm hover:font-semibold">Info</li>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { useSession, signIn, signOut } from "next-auth/react"
 import Navbar from "./components/Navbar";
 import { FaSellsy } from "react-icons/fa";
 import { MdOutlineFavorite } from "react-icons/md";
@@ -19,14 +18,14 @@ async function fetchData() {
 export default async function Home() {
   const { status, data } = await fetchData()
   return (
-    <main className="">
+    <main className="bg-gray-100">
       <Navbar />
       <div className="grid justify-center mt-14 sm:mt-16 lg:mt-20 gap-10 w-85/100 md:h-4/5 lg:w-3/4 mx-auto">
         <div className="banner grid sm:grid-cols-2 gap-8 sm:gap-0 md:items-center">
           <div className="left">
             <div className="caption grid gap-2 lg:gap-2.5">
               <h3 className="text-secondary text-3xl lg:text-5xl font-semibold mb-1 lg:mb-3">ALKA Cafe</h3>
-              <h3 className="text-white bg-secondary text-3xl lg:p-2.5 lg:text-5xl font-bold w-min">#CAFEKEKINIAN</h3>
+              <h3 className="text-white bg-secondary text-3xl py-1 px-1.5 sm:px-2 lg:p-2.5 lg:text-5xl font-bold w-min">#CAFEKEKINIAN</h3>
               <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur porro, illum reiciendis quisquam deleniti iusto accusantium vel quae perspiciatis nulla ducimus natus similique, quod id.</p>
               <button className="py-1 bg-secondary rounded-lg text-white font-semibold w-1/3">Home</button>
             </div>
@@ -40,32 +39,32 @@ export default async function Home() {
         <h3 className="text-secondary text-center text-lg lg:text-xl font-semibold lg:font-bold">Tentang Kami</h3>
         <p className="text-center font-normal">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo eius possimus non tempore voluptatum unde rerum voluptatem inventore sed modi. Fuga ea porro earum quasi.</p>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
-          <div className="card p-5 border-2 text-center border-secondary rounded-lg">
+          <div className="card bg-white p-5 border-2 text-center border-secondary rounded-lg">
             <FaSellsy className="text-7xl mb-1.5 text-secondary mx-auto" />
             <h4 className="font-bold">Ramai Pengunjung</h4>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
           </div>
-          <div className="card p-5 border-2 text-center border-secondary rounded-lg">
+          <div className="card bg-white p-5 border-2 text-center border-secondary rounded-lg">
             <MdOutlineFavorite className="text-7xl mb-1.5 text-secondary mx-auto" />
             <h4 className="font-bold">Favorit</h4>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
           </div>
-          <div className="card p-5 border-2 text-center border-secondary rounded-lg">
+          <div className="card bg-white p-5 border-2 text-center border-secondary rounded-lg">
             <SiNamecheap className="text-7xl mb-1.5 text-secondary mx-auto" />
             <h4 className="font-bold">Murah</h4>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
           </div>
-          <div className="card p-5 border-2 text-center border-secondary rounded-lg">
+          <div className="card bg-white p-5 border-2 text-center border-secondary rounded-lg">
             <GrServices className="text-7xl mb-1.5 text-secondary mx-auto" />
             <h4 className="font-bold">Pelayanan Terbaik</h4>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
           </div>
         </div>
       </div>
-      <div className="banner-lanscape relative grid justify-center mt-20 p-5 w-85/100 md:h-4/5 lg:w-3/4 mx-auto">
+      <div className="banner-lanscape relative grid justify-center mt-20 p-3 sm:p-5 lg:p-6 w-85/100 md:h-4/5 lg:w-3/4 mx-auto">
         <Image src={"/banner-lanscape.jpg"} alt="login" width={5000} height={5000} className="absolute h-48 sm:h-60 md:h-64 lg:h-64 xl:h-72 object-cover -scale-x-100" />
         <h3 className="text-white text-3xl lg:text-5xl font-semibold z-50">ALKA Cafe</h3>
-        <p className="text-white font-medium lg:font-semibold z-50 w-4/5 lg:w-2/3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolor doloremque soluta optio perspiciatis ipsa harum inventore modi nobis sequi.</p>
+        <p className="text-white font-medium lg:font-semibold z-50 w-90/100 lg:w-3/4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolor doloremque soluta optio perspiciatis ipsa harum inventore modi nobis sequi.</p>
       </div>
       <div className="product grid lg:gap-2 justify-center mt-16 sm:mt-32 md:mt-36 lg:mt-40 xl:mt-48 w-85/100 md:h-4/5 lg:w-3/4 mx-auto">
         <h3 className="text-secondary text-lg lg:text-xl font-semibold lg:font-bold text-center">Penjualan Terlaris</h3>
