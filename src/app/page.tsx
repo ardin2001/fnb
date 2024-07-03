ac94aabce2b190ba381dd1f97c2bca5e53737498
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-07-03 17:42:48 +0700

    debugging error vercel v5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,35 @@
-"use client";
-import Image from "next/image";
-import Navbar from "./components/User/Root/Navbar";
-import { FaSellsy } from "react-icons/fa";
-import { MdOutlineFavorite } from "react-icons/md";
-import { SiNamecheap } from "react-icons/si";
-import { GrServices } from "react-icons/gr";
-import CardProduct from "./components/User/Root/CardProduct";
-import CardMarket from "./components/User/Root/CardMarket";
-import Maps from "./components/User/Root/Maps";
-import { PiBuildingOffice } from "react-icons/pi";
-import { MdOutlineLocalPostOffice } from "react-icons/md";
-import { BsTelephone } from "react-icons/bs";
-import Link from "next/link";
-import Theme from "./components/Theme";
-import Banner from "./components/User/Root/Banner";
-import Footer from "./components/User/Root/Footer";
+// import Image from "next/image";
+// import Navbar from "./components/User/Root/Navbar";
+// import { FaSellsy } from "react-icons/fa";
+// import { MdOutlineFavorite } from "react-icons/md";
+// import { SiNamecheap } from "react-icons/si";
+// import { GrServices } from "react-icons/gr";
+// import CardProduct from "./components/User/Root/CardProduct";
+// import CardMarket from "./components/User/Root/CardMarket";
+// import Maps from "./components/User/Root/Maps";
+// import { PiBuildingOffice } from "react-icons/pi";
+// import { MdOutlineLocalPostOffice } from "react-icons/md";
+// import { BsTelephone } from "react-icons/bs";
+// import Link from "next/link";
+// import Theme from "./components/Theme";
+// import Banner from "./components/User/Root/Banner";
+// import Footer from "./components/User/Root/Footer";
 
-async function fetchData() {
-  // default : cache : 'force-cache'
-  const response = await fetch(`${process.env.HOSTNAME_P1}/api/products`, {
-    cache: 'no-store'
-  })
-  const { status, data } = await response.json()
-  return { status, data }
-}
+// async function fetchData() {
+//   // default : cache : 'force-cache'
+//   const response = await fetch(`${process.env.HOSTNAME_P1}/api/products`, {
+//     cache: 'no-store'
+//   })
+//   const { status, data } = await response.json()
+//   return { status, data }
+// }
 
 export default async function Home() {
-  const { status, data } = await fetchData()
+  // const { status, data } = await fetchData()
   return (
     <main className="bg-low">
-      <Navbar />
+      <p>tester browwwwwwwwwwwww</p>
+      {/* <Navbar />
       <div className="grid justify-center mt-14 sm:mt-16 lg:mt-20 gap-10 w-85/100 md:h-4/5 lg:w-3/4 mx-auto">
         <div className="banner grid sm:grid-cols-2 gap-8 sm:gap-0 md:items-center">
           <div className="left">
@@ -119,7 +119,7 @@ export default async function Home() {
         </div>
       </div>
       <Theme classname="fixed bottom-2.5 right-2.5 grid place-content-center bg-secondary text-white drop-shadow-strong rounded-full p-2" />
-      <Footer />
+      <Footer /> */}
     </main>
   );
 }
