30273e9b5d34c0bd5df7d6b3f9de6dec54c785c7
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-07-03 20:54:01 +0700

    update fix sidebar

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -23,7 +23,7 @@ export default function DashboardLayout({
 
     return (
         <main className="flex bg-white">
-            <Sidebar setOpen={() => setOpen((prev: any) => !prev)} classname={`${open ? "left-0 duration-500 z-50" : "-left-80 duration-500"} ease-in w-64 fixed h-screen px-6 pt-4 pb-14 flex flex-col justify-between bg-white shadow-furdamental shadow-gray-300 overflow-y-auto lg:w-80 lg:sticky`} />
+            <Sidebar setOpen={() => setOpen((prev: any) => !prev)} classname={`${open ? "left-0 duration-500 z-50" : "-left-80 duration-500"} ease-in w-64 fixed h-screen px-6 pt-4 pb-20 lg:pb-6 flex flex-col justify-between bg-white shadow-furdamental shadow-gray-300 overflow-y-auto lg:w-80 lg:sticky`} />
             <div className={`${open ? "duration-500 blur-medium" : null} content w-full px-5 py-3 sm:px-7 sm:py-4 md:px-8 flex flex-col gap-2 sm:gap-2.5 lg:gap-3 lg:blur-none`} onClick={HandlerMain}>
                 <TopBar classname="flex justify-between items-center" setOpen={() => setOpen((prev: any) => !prev)} title={title} />
                 {children}
