9eae76945f9484d9b3b69b9ddbe4f6a1cd5ba5fe
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-08-03 21:35:21 +0700

    update

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import Footer from "../components/Footer";
+import { FaWhatsapp } from "react-icons/fa";
+import CardProduct from "./cardProduct";
+import { IoFlash } from "react-icons/io5";
 
 async function fetchData() {
   // default : cache : 'force-cache'
   try {
-    const response = await fetch(`${process.env.HOSTNAME_P1}/api/products?limit=4&page=1&order=rating&sort=desc`, {
+    const response = await fetch(`${process.env.HOSTNAME_P1}/api/products`, {
       cache: 'no-store'
     })
     const { status, data } = await response.json()
@@ -16,7 +19,7 @@ async function fetchData() {
 }
 
 export default async function Home() {
-  // const { status, data } = await fetchData()
+  const { status, data } = await fetchData()
   return (
     <main className="bg-low">
       <div className="mt-2 sm:mt-2.5 lg:mt-3 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 mx-auto mb-10 relative">
@@ -74,9 +77,37 @@ export default async function Home() {
           </div>
         </div>
       </div>
-      <div className="discount px-4 sm:px-6 md:px-8 lg:px-10 ">
-
+      <div className="discount mt-14 sm:mt-20 lg:mt-24 px-6 sm:px-8 md:px-10 lg:px-12 xl:px-14 flex flex-col sm:flex-row">
+        <div className="description sm:w-1/3">
+          <div className="top mb-2 sm:mb-2.5 md:mb-3 lg:mb-4 xl:mb-5">
+            <div className="flex items-center mb-1 sm:mb-1.5 gap-1.5 sm:gap-2">
+              <IoFlash className="text-white bg-secondary p-2 rounded-all font-semibold text-3xl sm:text-4xl" />
+              <h3 className="text-xl sm:text-2xl font-semibold">Flash Deal</h3>
+            </div>
+            <p className="text-gray-600 text-lg">Berakhir Dalam 00 : 10 : 04</p>
+          </div>
+          <div className="bottom grid gap-2">
+            <h4 className="text-secondary text-xl sm:text-2xl font-medium sm:font-semibold">Diskon tiap hari sampai 70%!</h4>
+            <p className="text-sm text-gray-600 w-90/100">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit nulla error tempora consequuntur possimus aut autem!</p>
+            <Link href="/menu" className="btn-primary w-max bg-secondary py-1.5 px-3.5 sm:px-4 md:px-5 rounded-md text-white font-medium sm:font-semibold">Order Now</Link>
+          </div>
+        </div>
+        <div className="product-discount overflow-auto w-full">
+          <div className="flex w-max">
+            {data.map((item: any) => <CardProduct key={item.id} product={item} />)}
+          </div>
+        </div>
       </div>
+      {/* <div className="reservation mt-16 sm:mt-20 md:mt-28 h-44 bg-tertiary mx-6 sm:mx-8 md:mx-10 lg:mx-12 xl:mx-14 grid md:grid-cols-2">
+        <Image src="/cafe.avif" alt="reservation" width={1000} height={1000} className="w-96 h-80 object-cover" />
+        <div className="caption">
+          <h3>Reservation</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere cupiditate quibusdam dicta.</p>
+          <div className="btn-wa">
+            <Link href="https://wa.me/6289530522228" className="py-1 lg:py-2 rounded-lg text-white border-2 border-white font-semibold w-min px-4 lg:px-5 flex items-center gap-1 sm:gap-1.5"><FaWhatsapp /> whatsapp</Link>
+          </div>
+        </div>
+      </div> */}
       <Footer />
     </main>
   );
